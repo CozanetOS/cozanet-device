007bba7fb14ca7e17a4719d40fdecf8473c08355
CozyCrypto <fassdavid722@gmail.com>
2026-07-11 01:21:37 +0100

    feat: scaffold src/index.ts

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export * from './types';
+export * from './Registry/registry';
+export * from './Monitoring/monitor';
+export * from './Notifications/notifications';
+export * from './Storage/storage';
+export * from './Sync/sync';
+export * from './Clipboard/clipboard';
